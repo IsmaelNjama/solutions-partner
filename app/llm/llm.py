9fe08fc650d6fdece2444a295f5d13cf666fd4f4
IsmaelNjama <petermailsnjama@gmail.com>
2026-08-18 19:03:17 +0300

    Fix llm api error

--- app/llm/llm.py
+++ app/llm/llm.py
@@ -1,4 +1,3 @@
-import json
 import os
 
 from dotenv import load_dotenv
@@ -8,24 +7,15 @@ from app.tools.tools import tools
 
 load_dotenv()
 
-secret_string = os.getenv("NEBIUS_API_KEY")
+NEBIUS_API_KEY = os.getenv("NEBIUS_API_KEY")
 
-if not secret_string:
-    raise RuntimeError("NEBIUS_API_KEY is not set")
-
-
-try:
-    parsed = json.loads(secret_string)
-    NEBIUS_API_KEY = parsed["NEBIUS_API_KEY"]
-except (json.JSONDecodeError, TypeError, KeyError):
-    NEBIUS_API_KEY = secret_string
 
 llm = ChatOpenAI(
     model="Qwen/Qwen3-235B-A22B-Instruct-2507",
     base_url="https://api.tokenfactory.us-central1.nebius.com/v1/",
     api_key=NEBIUS_API_KEY,
     stream_usage=True,
-    streaming=True,
+    streaming=True,  # Explicitly enforce streaming
 )
 
 llm_with_tools = llm.bind_tools(tools)
